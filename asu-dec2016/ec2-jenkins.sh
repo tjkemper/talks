3391f62af47d9c1811bdcbc7000b116819de4fae
Taylor Kemper <tjkemper6@gmail.com>
2016-11-22 12:46:46 -0500

    add ec2 jenkins script

---
+++ asu-dec2016/ec2-jenkins.sh
@@ -0,0 +1,7 @@
+
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get install -y openjdk-8-jdk && sudo apt-get install -y maven && sudo apt-get install -y git-all
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get install -y jenkins
+
