8ae160c2aaf580c7a6fac3ff7f614be1c845fbb7
Taylor Kemper <tjkemper@users.noreply.github.com>
2017-05-10 22:54:43 -0400

    update user data - not working

--- asu-dec2016/user-data.sh
+++ asu-dec2016/user-data.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-apt-get -y update && apt-get -y upgrade && apt-get install -y openjdk-8-jdk && apt-get install -y maven && apt-get install -y git-all && wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add - && sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' && apt-get install -y jenkins
+# not working
+
+apt-get -y update 
+apt-get install -y openjdk-8-jdk 
+apt-get install -y maven 
+apt-get install -y git-all 
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add - 
+echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list 
+apt-get install -y jenkins 
+service jenkins status
