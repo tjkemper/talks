f9ba098dea5c95d077b9524d75278f883bf93856
Taylor Kemper <tjkemper@users.noreply.github.com>
2017-05-10 22:44:57 -0400

    fix jenkins installation

--- asu-dec2016/ec2-setup.sh
+++ asu-dec2016/ec2-setup.sh
@@ -1,1 +1,8 @@
-sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get install -y openjdk-8-jdk && sudo apt-get install -y maven && sudo apt-get install -y git-all && wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add - && sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' && sudo apt-get install -y jenkins
+sudo apt-get -y update 
+sudo apt-get install -y openjdk-8-jdk 
+sudo apt-get install -y maven 
+sudo apt-get install -y git-all 
+sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add - 
+sudo echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list 
+sudo apt-get install -y jenkins 
+sudo service jenkins status
